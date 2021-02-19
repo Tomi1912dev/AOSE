3ad3759dbe8bc218d57c30698a916c251eae3b25
Salome9709 <salomeattar@gmail.com>
2021-02-19 20:49:15 +0100

    get of the preferences and the status of the consumer

--- src/test/Sceanario1Test.java
+++ src/test/Sceanario1Test.java
@@ -5,6 +5,7 @@ import consumer.Policy;
 import consumer.Preference;
 import energy.Energy;
 import energy.Type;
+import interfaces.ConsumerManager;
 import interfaces.SystemAgentManager;
 import jade.core.Profile;
 import jade.core.ProfileImpl;
@@ -20,7 +21,7 @@ import producer.ProducerAgent;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ChooseProducerBehaviourTest {
+class Sceanario1Test {
 
     @Test
     void chooseProducerBehaviourTest() {
@@ -51,26 +52,24 @@ class ChooseProducerBehaviourTest {
 
             SystemAgentManager o2a = null;
             SystemAgentManager o2a1 = null;
-            SystemAgentManager o2o2 = null;
+            ConsumerManager o2o2 = null;
 
 
             try{
                 o2a = systemAgent.getO2AInterface(SystemAgentManager.class);
                 o2a1 = producerAgent.getO2AInterface(SystemAgentManager.class);
-                o2o2 = consumerAgent.getO2AInterface(SystemAgentManager.class);
+                o2o2 = consumerAgent.getO2AInterface(ConsumerManager.class);
             }catch(StaleProxyException e){e.printStackTrace();}
             assert(o2a!=null);
             assert(o2a1!=null);
-            //assert(o2o2!=null);
+            assert(o2o2!=null);
             Thread.sleep(30000);
+            System.out.println(o2a.toString());
             System.out.println(o2a1.toString());
-            //assertEquals(o2a.toString(),"[[0, RENEWABLE, 127.92, 8h/10h, "+producerAgent.getName()+"][1, CLASSIC, 127.2, 14h/18h, ProducerAgent@172.17.0.1:8889/JADE][2, CLASSIC, 127.16, 9h/10h, ProducerAgent@172.17.0.1:8889/JADE][3, RENEWABLE, 139.21, 14h/17h, ProducerAgent@172.17.0.1:8889/JADE]]");
-            //assertEquals(o2a1.toString(),);
-            //System.out.println(o2o2.getStatus());
+            System.out.println(o2o2.getOrder().getStatus().toString());
 
-            //State s = consumerAgent.getState();
-            //System.out.println(s.toString());
-           // assertEquals(s.toString(),"Initiated(1)");
+
+
 
         } catch (StaleProxyException | InterruptedException e) { e.printStackTrace(); }
     }
